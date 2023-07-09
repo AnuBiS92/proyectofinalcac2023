8fbc1bf4d483cee40a68cd6ad90889bf279b46aa
Emiliano Alvarez <64234452+AnuBiS92@users.noreply.github.com>
2023-07-09 16:40:06 -0300

    fix: 'Pedido.html'

--- Backend/api.py
+++ Backend/api.py
@@ -4,17 +4,19 @@ from flask_cors import CORS
 
 DATABASE = 'proyectofinal'
 
+
 def get_db_connection():
     conn = sqlite3.connect(DATABASE)
     conn.row_factory = sqlite3.Row
     return conn
+
 
 def create_table():
     conn = get_db_connection()
     cursor = conn.cursor()
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS carta (
-            codigo INTEGER PRIMARY KEY,
+            codigo INTEGER PRIMARY KEY AUTOINCREMENT,
             nombre TEXT NOT NULL,
             descripcion TEXT NOT NULL,
             url TEXT NOT NULL,
@@ -25,15 +27,18 @@ def create_table():
     cursor.close()
     conn.close()
 
+
 def create_database():
     conn = sqlite3.connect(DATABASE)
     conn.close()
     create_table()
 
+
 create_database()
 
+
 class Plato:
-    def __init__(self, codigo, nombre, descripcion, url, precio, cantidad = 0):
+    def __init__(self, codigo, nombre, descripcion, url, precio, cantidad=0):
         self.codigo = codigo
         self.nombre = nombre
         self.descripcion = descripcion
@@ -47,21 +52,15 @@ class Plato:
         self.url = nueva_imagen
         self.precio = nuevo_precio
 
+
 class Carta:
     def __init__(self):
         self.conexion = get_db_connection()
         self.cursor = self.conexion.cursor()
-        self.platos = []
 
-    def agregar_plato(self, codigo, nombre, descripcion, url, precio):
-        plato_existente = self.consultar_plato(codigo)
-        if plato_existente:
-            return jsonify({'message': 'Ya existe un plato con ese código.'}), 400
-
-        nuevo_plato = Plato(codigo, nombre, descripcion, url, precio)
-        self.platos.append(nuevo_plato)
-        self.cursor.execute("INSERT INTO carta VALUES (?, ?, ?, ?,?)",
-                            (codigo, nombre, descripcion, url, precio))
+    def agregar_plato(self, nombre, descripcion, url, precio):
+        self.cursor.execute("INSERT INTO carta (nombre, descripcion, url, precio) VALUES (?, ?, ?,?)",
+                            (nombre, descripcion, url, precio))
         self.conexion.commit()
         return jsonify({'message': 'Plato añadido correctamente.'}), 200
 
@@ -77,7 +76,8 @@ class Carta:
     def modificar_plato(self, codigo, nuevo_nombre, nueva_descripcion, nueva_url, nuevo_precio):
         plato = self.consultar_plato(codigo)
         if plato:
-            plato.modificar(nuevo_nombre, nueva_descripcion, nueva_url, nuevo_precio)
+            plato.modificar(nuevo_nombre, nueva_descripcion,
+                            nueva_url, nuevo_precio)
             self.cursor.execute("UPDATE carta SET nombre= ?, descripcion = ?, url = ?, precio = ? WHERE codigo = ?",
                                 (nuevo_nombre, nueva_descripcion, nueva_url, nuevo_precio, codigo))
             self.conexion.commit()
@@ -90,8 +90,7 @@ class Carta:
         platos = []
         for row in rows:
             codigo, nombre, descripcion, url, precio = row
-            plato = {'codigo': codigo, 'nombre': nombre, 'descripcion': descripcion,
-                        'URL': url, 'precio': precio}
+            plato = {'codigo': codigo, 'nombre': nombre, 'descripcion': descripcion, 'URL': url, 'precio': precio}
             platos.append(plato)
         return jsonify(platos), 200
 
@@ -103,8 +102,10 @@ class Carta:
             return jsonify({'message': 'Plato eliminado correctamente.'}), 200
         return jsonify({'message': 'Plato no encontrado.'}), 404
 
+
 class Pedido:
     items = []
+
     def agregar(self, codigo, cantidad, carta):
         plato = carta.consultar_plato(codigo)
         for item in self.items:
@@ -112,7 +113,7 @@ class Pedido:
                 item.cantidad += cantidad
                 return jsonify({'message': 'Plato agregado al carrito correctamente.'}), 200
 
-        nuevo_item = Plato(codigo, plato.nombre, plato.descripcion, plato.precio, cantidad)
+        nuevo_item = Plato(codigo, plato.nombre, plato.descripcion, plato.url, plato.precio, cantidad)
         self.items.append(nuevo_item)
         return jsonify({'message': 'Plato agregado al carrito correctamente.'}), 200
 
@@ -130,16 +131,17 @@ class Pedido:
     def mostrar(self):
         platos_pedido = []
         for item in self.items:
-            plato = {'codigo': item.codigo, 'nombre': item.nombre, 'descripcion': item.descripcion, 'URL': item.url, 'cantidad': item.cantidad,
-                        'precio': item.precio}
+            plato = {'codigo': item.codigo, 'nombre': item.nombre, 'descripcion': item.descripcion, 'URL': item.url, 'cantidad': item.cantidad, 'precio': item.precio}
             platos_pedido.append(plato)
         return jsonify(platos_pedido), 200
+
 
 app = Flask(__name__)
 CORS(app)
 
 pedido = Pedido()
 carta = Carta()
+
 
 @app.route('/platos/<int:codigo>', methods=['GET'])
 def obtener_plato(codigo):
@@ -154,18 +156,21 @@ def obtener_plato(codigo):
         }), 200
     return jsonify({'message': 'Producto no encontrado.'}), 404
 
+
 @app.route('/platos', methods=['GET'])
 def obtener_platos():
     return carta.listar_platos()
 
+
 @app.route('/platos', methods=['POST'])
 def agregar_plato():
-    codigo = request.json.get('codigo')
+    # codigo = request.json.get('codigo')
     nombre = request.json.get('nombre')
     descripcion = request.json.get('descripcion')
     url = request.json.get('url')
     precio = request.json.get('precio')
-    return carta.agregar_plato(codigo, nombre, descripcion, url , precio)
+    return carta.agregar_plato(nombre, descripcion, url, precio)
+
 
 @app.route('/platos/<int:codigo>', methods=['PUT'])
 def modificar_plato(codigo):
@@ -175,9 +180,11 @@ def modificar_plato(codigo):
     nuevo_precio = request.json.get('precio')
     return carta.modificar_plato(codigo, nuevo_nombre, nueva_descripcion, nueva_url, nuevo_precio)
 
+
 @app.route('/platos/<int:codigo>', methods=['DELETE'])
 def eliminar_plato(codigo):
     return carta.eliminar_plato(codigo)
+
 
 @app.route('/pedido', methods=['POST'])
 def agregar_item():
@@ -186,19 +193,23 @@ def agregar_item():
     carta = Carta()
     return pedido.agregar(codigo, cantidad, carta)
 
+
 @app.route('/pedido', methods=['DELETE'])
 def quitar_item():
     codigo = request.json.get('codigo')
     cantidad = request.json.get('cantidad')
     return pedido.quitar(codigo, cantidad)
 
+
 @app.route('/pedido', methods=['GET'])
 def obtener_pedido():
     return pedido.mostrar()
 
+
 @app.route('/')
 def index():
     return 'API del CRUD de platos'
+
 
 if __name__ == '__main__':
     app.run()
